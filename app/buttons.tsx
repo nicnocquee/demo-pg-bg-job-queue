b998ee43b44ea4032297dead2a3a536186baf571
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2025-06-28 22:02:58 +0200

    Refactor button click handlers to use startTransition for improved responsiveness during async operations. Update to consistent double quotes in JSX attributes.

--- app/buttons.tsx
+++ app/buttons.tsx
@@ -1,41 +1,52 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { sendEmail } from './jobs/email';
-import { generateReport } from './jobs/report';
-import { useTransition } from 'react';
-import { cancelPendingJobs } from './jobs/cancel';
+import { Button } from "@/components/ui/button";
+import { sendEmail } from "./jobs/email";
+import { generateReport } from "./jobs/report";
+import { useTransition } from "react";
+import { cancelPendingJobs } from "./jobs/cancel";
 
 export default function Buttons() {
-  const [isPending] = useTransition();
+  const [isPending, startTransition] = useTransition();
   return (
     <div className="flex flex-row gap-2">
       <Button
         disabled={isPending}
-        onClick={async () => {
-          const job = await sendEmail({
-            name: `John Doe ${Date.now()}`,
-            email: `john.doe${Date.now()}@example.com`,
+        onClick={() => {
+          startTransition(async () => {
+            const job = await sendEmail({
+              name: `John Doe ${Date.now()}`,
+              email: `john.doe${Date.now()}@example.com`,
+            });
+            console.log(job);
           });
-          console.log(job);
         }}
       >
         Send E-mail
       </Button>
       <Button
         disabled={isPending}
-        onClick={async () => {
-          const job = await generateReport({
-            name: `John Doe ${Date.now()}`,
-            email: `john.doe${Date.now()}@example.com`,
-            reportId: `report-${Date.now()}`,
+        onClick={() => {
+          startTransition(async () => {
+            const job = await generateReport({
+              name: `John Doe ${Date.now()}`,
+              email: `john.doe${Date.now()}@example.com`,
+              reportId: `report-${Date.now()}`,
+            });
+            console.log(job);
           });
-          console.log(job);
         }}
       >
         Generate Report
       </Button>
-      <Button onClick={cancelPendingJobs} disabled={isPending}>
+      <Button
+        onClick={() => {
+          startTransition(async () => {
+            await cancelPendingJobs();
+          });
+        }}
+        disabled={isPending}
+      >
         Cancel Pending Jobs
       </Button>
     </div>
